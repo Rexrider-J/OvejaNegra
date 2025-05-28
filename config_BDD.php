a05d2717a753bcbc3593f6a0edbd87c0b0a2e3c9
Tom <ticisternas@gmail.com>
2025-05-28 00:14:06 -0300

    nada significativo

--- config_BDD.php
+++ config_BDD.php
@@ -5,10 +5,9 @@ $contrasena = "";  // Contraseña del usuario (vacía si no se ha configurado)
 $base_de_datos = "oveja_negra"; // Nombre de la base de datos a la que se conectará
 
 // Crear una conexión a MySQL usando MySQLi
-$conn = new mysqli($host, $usuario, $contrasena, $base_de_datos);
+$conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
 
 // Verificar si ocurrió un error en la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error); // Mostrar mensaje y detener ejecución si falla
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error); // Mostrar mensaje y detener ejecución si falla
 }
-?>
