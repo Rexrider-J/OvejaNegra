91c2ec4c01c8edca372363c44e62c0b77a88a3c3
Tom <ticisternas@gmail.com>
2025-06-11 12:40:28 -0300

    modificacion en la conexion a la base de datos para que los caracteres sean utf8

--- config_BDD.php
+++ config_BDD.php
@@ -7,6 +7,8 @@ $base_de_datos = "oveja_negra"; // Nombre de la base de datos a la que se conect
 // Crear una conexión a MySQL usando MySQLi
 $conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
 
+$conexion->set_charset("utf8");
+
 // Verificar si ocurrió un error en la conexión
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error); // Mostrar mensaje y detener ejecución si falla
